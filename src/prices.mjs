fb5b30d600a3d1aef9b5556f9e3df24755cd7a5b
王昱 <wangyu2021@foxmail.com>
2025-04-25 16:25:07 +0800

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -86,6 +86,7 @@ function createApp(database) {
     console.log(date, holidays);
     for (let row of holidays) {
       let holiday = new Date(row.holiday);
+      console.log(row.holiday, holiday);
       if (
         date &&
         date.getFullYear() === holiday.getFullYear() &&
