6dc6bf31f1eb8e0717c98aa754aaeabe88768b88
王昱 <wangyu2021@foxmail.com>
2025-04-25 16:39:28 +0800

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -87,7 +87,6 @@ function createApp(database) {
     for (let row of holidays) {
       let holiday = date instanceof Date ? new Date(row.holiday) : Temporal.PlainDate.from(row.holiday);
       let holiday2 = Temporal.PlainDate.from(row.holiday);
-      console.log(row.holiday, holiday, holiday2);
       if (
         date &&
         date instanceof Date &&
