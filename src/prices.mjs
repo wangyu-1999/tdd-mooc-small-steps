1333a555cee8a0468af110f04256c3f7ff343321
王昱 <wangyu2021@foxmail.com>
2025-04-25 16:23:27 +0800

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -83,6 +83,7 @@ function createApp(database) {
 
   function isHoliday(date) {
     const holidays = database.getHolidays();
+    console.log(date, holidays);
     for (let row of holidays) {
       let holiday = new Date(row.holiday);
       if (
