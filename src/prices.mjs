05c264764321dabd28b33bb6cc19715b6c20342e
王昱 <wangyu2021@foxmail.com>
2025-04-25 16:53:01 +0800

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -78,7 +78,7 @@ function createApp(database) {
     } else {
       date2 = undefined;
     }
-    console.log(date && isMonday(date) && !isHoliday(date));
+    if (date) console.log(date, isMonday(date), !isHoliday(date));
     console.log(date2 && isMonday(date2) && !isHoliday(date2));
     let reduction = 0;
     if (date && isMonday(date) && !isHoliday(date)) {
