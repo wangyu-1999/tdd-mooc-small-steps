fb0616e3ec221b94d1190b55dbe66414eb1a44a8
王昱 <wangyu2021@foxmail.com>
2025-04-25 17:07:47 +0800

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -34,7 +34,7 @@ function createApp(database) {
 
   function parseDate(dateString) {
     if (dateString) {
-      return new Date(dateString);
+      return Temporal.PlainDate.from(dateString);
     }
   }
 
